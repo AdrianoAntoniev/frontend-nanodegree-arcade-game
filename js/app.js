045569d7f2e4993ebad6cd52acdce9013674e269
Adriano <Adriano@>
2017-10-16 00:16:33 -0200

    Implementação de limitação de bordas do personagem.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 50;
+    this.y = 50;
+    this.speed = 10;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //this.x = this.x * dt;
+    var movement = 100 * dt;    
+    this.x = this.x + movement;
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +31,47 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {    
+    this.sprite = 'images/char-boy.png';
+    this.x = 101;
+    this.y = 400;    
+}
 
+Player.prototype.update = function() {        
 
+}
+
+Player.prototype.render = function() {    
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    console.log('(' + this.x + ', ' + this.y + ')');
+    if(key === 'right') { 
+        if(this.x < 403)         
+            this.x += 101;
+    } else if(key === 'left') {        
+        if(this.x > 1)
+            this.x -= 101;
+    } else if(key === 'up') {
+        if(this.y > -14)
+            this.y -= 83;
+    } else if(key == 'down') {
+        if(this.y < 399) 
+            this.y += 83;
+    }    
+    this.update();
+    this.render();
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player();
+var allEnemies = [];
 
+for(var i=0; i<5; i++) {
+    allEnemies.push(new Enemy());
+}
 
 
 // This listens for key presses and sends the keys to your
@@ -41,6 +83,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
